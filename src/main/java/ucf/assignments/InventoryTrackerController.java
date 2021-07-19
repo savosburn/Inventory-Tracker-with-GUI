c442496d38c4cc584cc4f830610055b03a56e8dc
savosburn <savosburn18@tampabay.rr.com>
2021-07-19 18:21:16 -0400

    save/load .txt files

--- src/main/java/ucf/assignments/InventoryTrackerController.java
+++ src/main/java/ucf/assignments/InventoryTrackerController.java
@@ -5,9 +5,7 @@
 
 package ucf.assignments;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
+import java.io.*;
 import java.net.URL;
 import java.text.NumberFormat;
 import java.util.*;
@@ -335,16 +333,38 @@ public class InventoryTrackerController {
         System.out.print("File Menu Opened.\n");
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     @FXML
     void loadButtonClicked(ActionEvent event) {
-
-        /*
 
         // File chooser opens
         //Window stage = fileMenuButton.getScene().getWindow();
         Window stage = fileMenuButton.getScene().getWindow();
         fileChooser.setTitle("Load To Do List");
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text File", "*.txt"));
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("TSV (Text) File", "*.txt"),
+                new FileChooser.ExtensionFilter("JSON File", "*.json"),
+                new FileChooser.ExtensionFilter("HTML File", "*.html")
+        );
 
         try {
             // User chooses file
@@ -356,19 +376,124 @@ public class InventoryTrackerController {
 
             // Chosen file is loaded
             // TODO UNCOMMENT THESSE NEXT TWO LINES
+
+            if (file.toString().contains(".txt")) {
+
+                ArrayList<String> inventoryStrings = loadFileStrings(file);
+
+                inventoryItems = addToObservableList(inventoryStrings);
+            }
+
+
             //ArrayList<String> fileStrings = loadFileStrings(file);
            // ObservableList<InventoryItem> loadedFile = addToObservableList(fileStrings);
 
             // Refresh and reset the table
-           // inventoryTrackerTable.refresh();
-            //inventoryTrackerTable.setItems(loadedFile);
+            inventoryTrackerTable.refresh();
+            inventoryTrackerTable.setItems(inventoryItems);
         } catch (Exception e) {
 
             // Check if file could be loaded
             System.out.print("Could not load file.\n");
-        } */
-
+        }
     }
+
+    // Post-conditions: Parses the strings in the ArrayList and returns them as an ObservableList
+    public ObservableList<InventoryItem> addToObservableList(ArrayList<String> items) {
+        ObservableList<InventoryItem> tempList = FXCollections.observableArrayList();
+
+        // Iterate through every object string
+        for (String item : items) {
+
+            // Parse each string
+            String[] parsedString = parseStrings(item);
+
+            System.out.print(parsedString[0]);
+            System.out.print(parsedString[1]);
+            System.out.print(parsedString[2]);
+
+            // Add the string to the observablelist
+
+            InventoryItem inventory = new InventoryItem();
+            inventory.setItemName(parsedString[0]);
+            inventory.setItemSerialNumber(parsedString[1]);
+            inventory.setItemPrice(parsedString[2]);
+
+            tempList.add(inventory);
+
+
+            //tempList = setItems(inventoryItems, parsedString[0], parsedString[1], parsedString[3]);
+        }
+
+        return tempList;
+    }
+
+    // Post-conditions: Adds the strings to an ArrayList
+    public ArrayList<String> loadFileStrings(File file) {
+        // Create new ArrayList
+        ArrayList<String> items = new ArrayList<>();
+
+        try {
+            // Get the file and make it readable
+            File myFile = new File(file.getName());
+            Scanner scanner = new Scanner(myFile);
+
+            // Each line holds one object
+            while (scanner.hasNextLine()) {
+                items.add(scanner.nextLine());
+            }
+
+            return items;
+        } catch (FileNotFoundException e) {
+
+            // Check if the file was not found
+            System.out.print("File not found.\n");
+
+            return null;
+        }
+    }
+
+    public String[] parseStrings(String item) {
+        return item.split("\t");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    public ObservableList<InventoryItem> items(File file) {
+
+        ObservableList<InventoryItem> inventoryItem = FXCollections.observableArrayList();
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(file))) {
+
+            String line = null;
+
+            while ((line = fileReader.readLine()) != null)) {
+                String
+            }
+
+        } catch (Exception e) {
+            System.out.print("File not found.\n");
+        }
+
+    } */
 
     @FXML
     public Boolean removeItemButtonClicked(ActionEvent event) {
@@ -396,7 +521,6 @@ public class InventoryTrackerController {
             System.out.print("The item was not deleted.\n");
             return false;
         }
-
     }
 
     // Post-conditions: Removes item from Observable list and from Set
@@ -426,19 +550,68 @@ public class InventoryTrackerController {
         Window stage = fileMenuButton.getScene().getWindow();
         fileChooser.setTitle("Save Dialog");
         fileChooser.setInitialFileName("New File");
-
         fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Text File", "*.txt"),
+                new FileChooser.ExtensionFilter("TSV (Text) File", "*.txt"),
                 new FileChooser.ExtensionFilter("JSON File", "*.json"),
-                new FileChooser.ExtensionFilter("TSV File", "*.tsv")
+                new FileChooser.ExtensionFilter("HTML File", "*.html")
         );
 
         try {
+            // Let user name the file to save
             File file = fileChooser.showSaveDialog(stage);
+
+            // Save the chosen directory for the next time it opens
             fileChooser.setInitialDirectory(file.getParentFile()); // save the chosen directory
+            List<InventoryItem> saveList = observableToList(inventoryItems);
             // TODO SAVE THE FILE
+            if (file.toString().contains(".txt")) {
+                System.out.print(saveToTXT(file, saveList));
+            }
         } catch (Exception e) {
             System.out.print("File could not be opened.\n");
+        }
+
+    }
+
+    // Post-conditions: Converts observable list to a regular list
+    public List<InventoryItem> observableToList(ObservableList<InventoryItem> items) {
+
+        return new ArrayList<>(items);
+
+    }
+
+    public String saveToTXT(File file, List<InventoryItem> items) {
+        StringBuilder outputString = new StringBuilder(" ");
+
+
+        try(FileWriter writer = new FileWriter(file)) {
+
+            // Add all the objects to the list with \t to separate the items in each object
+            for (InventoryItem item : items) {
+
+                writer.write(item.itemName + "\t");
+                writer.write(item.itemSerialNumber + "\t");
+                writer.write(item.itemPrice + "\t");
+
+                // Newline signifies new object
+                writer.write("\n");
+
+                // Create strings for testing
+                if(outputString.toString().equals(" ")) {
+                    outputString = new StringBuilder(String.format("%s\t", item.itemName));
+                } else {
+                    outputString.append(String.format("%s\t", item.itemName));
+                }
+
+                outputString.append(String.format("%s\t", item.itemSerialNumber));
+                outputString.append(String.format("%s\t", item.itemPrice));
+                outputString.append("\n");
+            }
+
+            return outputString.toString();
+
+        } catch (IOException e) {
+            return "File does not exist.\n";
         }
     }
 
